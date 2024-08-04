97c175d882f98ad9ffb82ab0bc97c4cde3d5f414
mage <mariamgevorgyan9@gmail.com>
2024-08-04 20:48:43 +0200

    list of users

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,20 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../../dummy-users';
+import { DUMMY_USERS } from './../../dummy-users';
+import {
+  Component,
+  computed,
+  Input,
+  Output,
+  input,
+  EventEmitter,
+  output,
+} from '@angular/core';
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// type User = { id: string; avatar: string; name: string };
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,19 +23,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]); // signals for Angular version >16
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+  //SIGNALS for ANGULAR version 18
+  // avatar = input<string>('');
+  // name = input<string>();
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  //Different when using signals
-  //SIGNALS ONLY USED HERE
-  // get imagePath(){
-  //   // console.log( this.selectedUser.avatar);
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
 
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+  get imagePath() {
+    return '../../../assets/users/' + this.user.avatar;
+  }
   onSelectUser() {
-    console.log('clicked ');
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.user.id);
   }
 }
